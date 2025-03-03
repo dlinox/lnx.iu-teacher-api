10d2f409969710e5767cf1046f2128dab27e7572
dlinox <nearlino20@gmail.com>
2025-03-03 13:56:12 -0500

    pass

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,9 @@ return [
     'paths' => ['api/*'], // Aplica CORS solo a rutas API
     'allowed_methods' => ['*'], // Permitir todos los métodos
     'allowed_origins' => [
-        'http://34.55.61.47:81',
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:5175',
-        'http://34.55.61.47:83'
+        //todo los orígenes permitidos de cualquier puerto 34.55.61.47:
+        '/^http:\/\/34\.55\.61\.47(:\d+)?$/', // Permitir cualquier puerto en esta IP
+        '/^http:\/\/localhost(:\d+)?$/' // Permitir localhost en cualquier puerto
     ], // Solo estos orígenes pueden acceder a la API
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], // Permitir todos los encabezados
