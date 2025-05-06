4f6ce62fbd94bd8e0e876b2b3f6030478f621e19
Denis Lino Puma Ticona <dpumaticona@gmail.com>
2025-05-06 01:02:15 -0500

    v1.1

--- app/Modules/Group/Http/Controllers/GroupController.php
+++ app/Modules/Group/Http/Controllers/GroupController.php
@@ -375,7 +375,7 @@ class GroupController extends Controller
                 'format' => 'A4',
                 'margin_left' => 10,
                 'margin_right' => 10,
-                'margin_top' => 10,
+                'margin_top' => 32,
                 'margin_bottom' => 10,
                 'margin_header' => 5,
                 'margin_footer' => 5,
---
+++ resources/views/pdf/AcademicRecord/_header.blade.php
@@ -0,0 +1,21 @@
+<table style="width: 100%; border: none;">
+    <tr>
+        <td style="width: 20%;">
+            <img src="{{ asset('assets/img/logo-unap.png') }}" alt="Logo UNAP" style="width: 70px; height: auto; margin: 0; padding: 0;">
+        </td>
+        <td style="width: 60%; text-align: center;">
+            <h4>
+                UNIVERSIDAD NACIONAL DEL ALTIPLANO
+            </h4>
+            <h5 class="m-10" style="margin: 10px 0 0 0;">
+                INSTITUTO DE INFORMÁTICA
+            </h5>
+            <h4>
+                PUNO - PERÚ
+            </h4>
+        </td>
+        <td style="width: 20%; text-align: right;">
+            <img src="{{ asset('assets/img/logo-info.jpg') }}" alt="Logo INFOUNA" style="width: 70px; height: auto; margin: 0; padding: 0;">
+        </td>
+    </tr>
+</table>
